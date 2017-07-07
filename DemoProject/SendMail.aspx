c57dca8059e2eea5da7d24863227d55e9fb67d3b
hellonibuthomas <hellonibu@yahoo.com>
2017-07-07 15:00:56 +0530

    hhh

---
+++ DemoProject/SendMail.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SendMail.aspx.cs" Inherits="DemoProject.SendMail" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="ddlTemplate" runat="server">
+            <asp:ListItem Value="Select">
+                Select
+            </asp:ListItem>
+            <asp:ListItem Value="Template1">
+                Template1
+            </asp:ListItem>
+            <asp:ListItem Value="Template2">
+                Template1
+            </asp:ListItem>
+            <asp:ListItem Value="Template3">
+                Template1
+            </asp:ListItem>
+        </asp:DropDownList>
+        <asp:Button ID="btnSendMail" runat="server" Text="Send Mail" OnClick="btnSendMail_Click" style="height: 26px" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ DemoProject/SendMail.aspx.cs
@@ -0,0 +1,101 @@
+﻿using MailBee.SmtpMail;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DemoProject
+{
+    public partial class SendMail : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSendMail_Click(object sender, EventArgs e)
+        {
+            string ddltext = ddlTemplate.SelectedValue;
+
+            if (ddltext=="Select")
+            {
+                Response.Write("<script>alert('Please Select Template')</script>");
+            }
+            else if (ddltext=="Template1")
+            {
+                string body = ReadHtml(ddltext);
+               bool status= SendHtmlFormattedEmailMailbee("recepientEmail1@domain.com", "Subject", body);
+               if (status)
+               {
+                   Response.Write("<script>alert('Mail Sent')</script>");
+               }
+               else
+               {
+                   Response.Write("<script>alert('Some error while sending mail')</script>");
+               }
+            }
+            else if (ddltext=="Template2")
+            {
+                string body = ReadHtml(ddltext);
+                bool status = SendHtmlFormattedEmailMailbee("recepientEmail2@domain.com", "Subject", body);
+                if (status)
+                {
+                    Response.Write("<script>alert('Mail Sent')</script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Some error while sending mail')</script>");
+                }
+                
+            }else
+            {
+                string body = ReadHtml(ddltext);
+                bool status = SendHtmlFormattedEmailMailbee("recepientEmail3@domain.com", "Subject", body);
+                if (status)
+                {
+                    Response.Write("<script>alert('Mail Sent')</script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Some error while sending mail')</script>");
+                }
+            }
+        }
+        public bool SendHtmlFormattedEmailMailbee(string recepientEmail, string subject, string body)
+        {
+            try
+            {
+                MailBee.SmtpMail.Smtp.LicenseKey = "MN200-31F9F661F915F943F9D9C38AF266-7B6";//Demo Key
+                MailBee.SmtpMail.Smtp ObjSmtp = new MailBee.SmtpMail.Smtp();
+                ObjSmtp.Message.From.AsString = ConfigurationManager.AppSettings["UserName"].ToString();
+                ObjSmtp.Message.To.AsString = recepientEmail;
+                ObjSmtp.Message.Subject = subject;
+                ObjSmtp.Message.BodyHtmlText = body;
+                SmtpServer server = new SmtpServer(ConfigurationManager.AppSettings["Host"], ConfigurationManager.AppSettings["UserName"], ConfigurationManager.AppSettings["Password"]);
+                server.Port = Convert.ToInt32(ConfigurationManager.AppSettings["Port"]);
+                ObjSmtp.SmtpServers.Add(server);
+                ObjSmtp.Send();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+            
+        }
+        public string ReadHtml(string filename)
+        {
+            string body = string.Empty;
+            using (StreamReader reader = new StreamReader(Server.MapPath("~\\html\\"+filename+".html")))
+            {
+                body = reader.ReadToEnd();
+            }
+            return body;
+        }
+    }
+}
